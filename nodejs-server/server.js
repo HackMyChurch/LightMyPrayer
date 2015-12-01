9698c951be5bee0d368f5fe8eca225c202ead949
Pierre-Gilles Levallois <pgl@erasme.org>
2015-12-01 17:47:39 +0100

    Solution light d'upload des images avec nodejs

---
+++ nodejs-server/server.js
@@ -0,0 +1,34 @@
+var images_path     = "./lmp/upload/"
+var express         =       require("express");
+var multer          =       require('multer');
+var app             =       express();
+var upload      =   multer({ dest: './uploads/'});
+
+app.use(multer({ dest: images_path,
+    // rename: function (fieldname, filename) {
+    //     return filename+Date.now();
+    // },
+    onFileUploadStart: function (file) {
+        console.log(file.originalname + ' is starting ...');
+    },
+    onFileUploadComplete: function (file) {
+        console.log(file.fieldname + ' uploaded to  ' + file.path)
+    }
+}));
+
+// app.get('/',function(req,res){
+//       res.sendFile(__dirname + "/index.html");
+// });
+
+app.post('/lmp/image/upload',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    });
+});
+
+app.listen(9292,function(){
+    console.log("Working on port 9292");
+});
